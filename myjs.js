49c9eff2a3995e1bec7cdd7db1201256e852e8a7
SYOJI-24 <zm25087@josai.ac.jp>
2026-06-22 12:03:23 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,3 +1,8 @@
-alert('Hello,Osamu!');
+alert('Hello,Shoji!');
 alert(1+1);
 alert(Math.sprt(1+1));
+alert(2+3);
+alert('2'+'3');
+alert(Number('2')+Nunber('3'));
+alert('2'+3);
+alret(Nunber('2')+3);
